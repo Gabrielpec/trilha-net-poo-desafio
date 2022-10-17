a6b0cfe5260224bb263d1851ac0bb55070d3299a
Gabrielpec <gabpercam@gmail.com>
2022-10-17 16:15:21 -0300

    Desafio Poo conclusão

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("iPhone");
+Iphone Ip = new Iphone("3322-1100", "iPhone 13", "95959595", 6);
+
+Ip.Ligar();
+Ip.InstalarAplicativo("WhatsApp");
+Ip.ReceberLigacao();
+Ip.Infos();
+
+Console.WriteLine("Nokia");
+Nokia Nk = new Nokia("8922-4002", "Nokia Tijolo", "123456789", 1);
+
+Nk.Ligar();
+Nk.InstalarAplicativo("Cobra");
+Nk.ReceberLigacao();
+Nk.Infos();
